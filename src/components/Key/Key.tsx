2810a3ad4f86897cbad0fb4de6b44878e25e082c
ELISHA1994 <elishabello2014@gmail.com>
2021-02-02 05:22:54 +0100

    Mapping Real Keys to Virtual

--- src/components/Key/Key.tsx
+++ src/components/Key/Key.tsx
@@ -2,6 +2,7 @@ import React, {FunctionComponent, ReactEventHandler} from "react"
 import clsx from "clsx"
 import { NoteType } from "../../domain/note"
 import "./style.css"
+import {usePressObserver} from "../PressObserver";
 
 interface KeyProps {
     type: NoteType
@@ -14,9 +15,15 @@ interface KeyProps {
 
 export const Key: FunctionComponent<KeyProps> = (props) => {
     const { type, label, onDown, onUp, ...rest } = props
+
+    const pressed = usePressObserver({
+        watchKey: label,
+        onStartPress: onDown,
+        onFinishPress: onUp
+    })
     return (
         <button
-            className={clsx(`key key--${type}`)}
+            className={clsx(`key key--${type}`, pressed && "is-pressed")}
             type="button"
             onMouseDown={onDown}
             onMouseUp={onUp}
