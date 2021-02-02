c581dc649fe665ba47f49008507b70df64ae5f71
ELISHA1994 <elishabello2014@gmail.com>
2021-02-02 12:31:22 +0100

    Dynamically Loading Instruments

--- src/components/Keyboard/WithInstrument.tsx
+++ src/components/Keyboard/WithInstrument.tsx
@@ -1,15 +1,19 @@
-import React, { FunctionComponent } from "react"
+import React, {FunctionComponent, useEffect} from "react"
 import { useAudioContext } from "../AudioContextProvider"
 import { useSoundfont } from "../../adapters/Soundfont"
-import { useMount } from "../../utils/useMount"
+// import { useMount } from "../../utils/useMount"
 import { Keyboard } from "./Keyboard"
 import "./style.css"
+import {useInstrument} from "../../state/Instrument";
 
 export const KeyboardWithInstrument: FunctionComponent = () => {
     const AudioContext = useAudioContext()!
-    const { loading, play, stop, load } = useSoundfont({ AudioContext })
+    const { instrument } = useInstrument()
+    const { loading, current, play, stop, load } = useSoundfont({ AudioContext })
 
-    useMount(load)
+    useEffect(() => {
+        if (!loading && instrument !== current) load(instrument)
+    }, [load, loading, current, instrument])
 
     return <Keyboard loading={loading} play={play} stop={stop} />
 }
